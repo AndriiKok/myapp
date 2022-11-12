d5e88cb995150549b70c6ee4e9a878fcb53c37a5
Andrii Koktash <a.koktash@gmail.com>
2022-11-12 21:27:40 +0200

    Saturday fixes

---
+++ allData.js
@@ -0,0 +1,472 @@
+export const allData = [
+  {
+    brand: "Z BY ZELLA GIRL",
+    name: "Kids' Zella Girl Twisted Front Long Sleeve Tee",
+    price: 8.48,
+    size: "small",
+    productID: "kid-1",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/000e32be-0b2e-4235-acf1-db56f90cb031.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ADIDAS",
+    name: "Kids' Printed Track Jacket & Leggings Set",
+    price: 28.97,
+    size: "small",
+    productID: "kid-2",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/f647006c-b72f-4d50-8ce4-ae5b0833167b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ADIDAS",
+    name: "Kids' Hooded Top & Tights Set",
+    price: 25.97,
+    size: "small",
+    productID: "kid-3",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/cc91a5f6-bee1-4a44-bcdd-0bebfeba754f.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "THE NORTH FACE",
+    name: "Oso Zip Fleece Hoodie",
+    price: 42.97,
+    size: "small",
+    productID: "kid-4",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/929456a3-dcf8-4c02-ad04-f54411950eb5.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "THE NORTH FACE",
+    name: "Oso Zip Fleece Hoodie",
+    price: 42.97,
+    size: "small",
+    productID: "kid-5",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/639aa4b9-9005-48ff-8009-398500cf5746.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "THE NORTH FACE",
+    name: "Kids' Oso Fleece Zip Hoodie",
+    price: 58.97,
+    size: "small",
+    productID: "kid-6",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/d3b32853-48bd-4dd3-bedc-79ba17d5126f.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ADIDAS",
+    name: "Kids' Echo Tilt Logo Graphic Tee",
+    price: 11.97,
+    size: "samll",
+    productID: "kid-7",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/d21c5a3a-e497-4ba0-a734-fb86b8b0222c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "NIKE",
+    name: "Kids' Swoosh Split Graphic T-Shirt",
+    price: 12.97,
+    size: "small",
+    productID: "kid-8",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/2509002a-99ef-4a3e-9c5b-97628641e786.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ADIDAS",
+    name: "Kids' Innovation Long Sleeve Hooded T-Shirt",
+    price: 14.97,
+    size: "small",
+    productID: "kid-9",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/591706b3-5f2f-46c8-844d-5b89e3b0db5a.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "URBAN REPUBLIC",
+    name: "Soft Shell Jacket",
+    price: 25.97,
+    size: "small",
+    productID: "kid-10",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/bb23ecfe-ead5-4028-a72a-d51cdabeebd9.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "OPEN EDIT",
+    name: "Roomy Organic Cotton Hooded Jacket",
+    price: 11.97,
+    size: "small",
+    productID: "kid-11",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/aba5ba9a-f0cb-4636-bb41-d93307bc2b77.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "LEVI'S®",
+    name: "Kids' Batwing Logo Denim Trucker Jacket",
+    price: 34.97,
+    size: "small",
+    productID: "kid-12",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/e7b8cab1-d75e-48ed-bec6-e798c6a79f08.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "NIKE",
+    name: "Swoosh Oval T-Shirt",
+    price: 14.23,
+    size: "medium",
+    productID: "men-1",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/dd660d9b-ded6-4108-87d5-2c11ec087d93.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "adidas",
+    name: "Essentials Camo T-Shirt",
+    price: 14.24,
+    size: "large",
+    productID: "men-2",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/a9a96453-6689-42a7-b40f-16e21007ac8a.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "NIKE",
+    name: "Essential Dri-FIT Half Zip Golf Pullover",
+    price: 50.0,
+    size: "small",
+    productID: "men-3",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/d3f9ea02-8632-4b2b-b987-df2f11d3fec3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "TED BAKER LONDON",
+    name: "Jarrow Trim Fit Jacket",
+    price: 279.97,
+    size: "medium",
+    productID: "men-4",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/8b733478-4674-4b0b-8c05-7552f992a42d.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "SAVILE ROW CO",
+    name: "Plaid Knit Blazer",
+    price: 99.97,
+    size: "medium",
+    productID: "men-5",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/23de3d57-e19e-47e0-bd47-ae7eeca78469.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "BEN SHERMAN",
+    name: "Performance Zip Front Track Jacket",
+    price: 82.49,
+    size: "large",
+    productID: "men-6",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/e93aed6a-1997-4c0e-92c9-b1869b83722a.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "FREE COUNTRY",
+    name: "Water-Resistant Hooded Rain Jacket",
+    price: 22.48,
+    size: "medium",
+    productID: "men-7",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/697d7e48-1d8c-4ccb-8c3e-385240639707.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "MICHAEL KORS",
+    name: "Athens Puffer Vest",
+    price: 64.99,
+    size: "medium",
+    productID: "men-8",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/457fec3e-425e-4778-8580-dd541cad97f5.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "SLATE & STONE",
+    name: "SLATE AND STONE Woven Bomber Jacket",
+    price: 79.99,
+    size: "medium",
+    productID: "men-9",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/8cb4af73-a6da-4a01-88f2-80910091508b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "TRAVISMATHEW",
+    name: "Excursion Knit Polo",
+    price: 49.97,
+    size: "medium",
+    productID: "men-10",
+    category: "polo",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/27dda56a-7d3d-46f7-b420-38b26a90bf2f.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "BROOKS BROTHERS",
+    name: "Solid Pique Slim Fit Polo",
+    price: 29.97,
+    size: "small",
+    productID: "men-11",
+    category: "polo",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/5baac204-c6d3-4583-9c8b-df8a0f0145a7.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ORIGINAL PENGUIN",
+    name: "Knit Jersey Polo Shirt",
+    price: 34.97,
+    size: "medium",
+    productID: "men-12",
+    category: "polo",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/c10da704-a074-44e9-8077-341fd76a74d8.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "THE NORTH FACE",
+    name: "Box Pullover Hoodie",
+    price: 40.97,
+    size: "medium",
+    productID: "men-13",
+    category: "Sweater",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/698180a3-7034-4b44-9f85-e56e8a9a3a85.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "JOHN VARVATOS",
+    name: "Embroidered Zip Up Hoodie",
+    price: 99.99,
+    size: "medium",
+    productID: "men-14",
+    category: "Sweater",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/97fabe0e-1c73-4cbd-b769-f4e405128535.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "LUCKY BRAND",
+    name: "Burnout Zip Hoodie",
+    price: 39.97,
+    size: "large",
+    productID: "men-15",
+    category: "Sweater",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/82a94825-2ae9-4ee7-acbe-f18b17570c25.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "90 DEGREE BY REFLEX",
+    name: "Faux Cracked Leather High Rise Ankle Leggings",
+    price: 19.97,
+    size: "medium",
+    productID: "wmen-4",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/daa12e06-645a-4eae-97b6-51cacb15a8a9.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "MELROSE AND MARKET",
+    name: "High Neck Knit Tank Top",
+    price: 3.88,
+    size: "medium",
+    productID: "wmen-1",
+    category: "women-clothing",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/da42a261-5ff5-47c2-8030-46bf110c79bd.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "LOVESTITCH",
+    name: "Giuze Maxi Dress",
+    price: 14.98,
+    size: "regular",
+    productID: "wmen-2",
+    category: "women-clothing",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/71b3e3f0-dfed-4d32-9571-da793d5608bc.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "MAXSTUDIO",
+    name: "Pleated Midi Skirt",
+    price: 23.98,
+    size: "medium",
+    productID: "wmen-3",
+    category: "women-clothing",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/8dd407c3-96d5-425a-8c81-b67829d9237c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "Z BY ZELLA",
+    name: "Shooting Star Drape Collar Cardigan",
+    price: 29.97,
+    size: "large",
+    productID: "wmen-5",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/7649eb26-5df1-49db-b422-ad5105068c11.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ELECTRIC YOGA",
+    name: "Keep It Up Twist Front Tank",
+    price: 16.97,
+    size: "medium",
+    productID: "wmen-6",
+    category: "activeware",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/863383dd-bbe7-4bcb-82fb-c7eac47d9751.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "CALVIN KLEIN",
+    name: "Two Button Short Jacket",
+    price: 59.97,
+    size: "medium",
+    productID: "wmen-7",
+    category: "blazers",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/7fc36e8b-e2ed-47f2-990c-9b615aefbfd4.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "BB DAKOTA BY STEVE MADDEN",
+    name: "Plaid For You Blazer",
+    price: 47.97,
+    size: "medium",
+    productID: "wmen-8",
+    category: "blazers",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/22b6dfab-a78f-4aad-8f4a-73c01001b93d.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "BELLE AND BLOOM",
+    name: "Princess Polly Textured Blazer",
+    price: 65.98,
+    size: "medium",
+    productID: "wmen-9",
+    category: "blazers",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/534124f6-4678-490e-a79f-0a5d254af489.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "BLANKNYC",
+    name: "Faux Leather Jacket",
+    price: 59.97,
+    size: "small",
+    productID: "wmen-10",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/0ddade09-84dc-4076-a371-d755dbba39c4.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "GUESS",
+    name: "Removable Faux Fur Collar Wool Blend Double Breasted Walker Coat",
+    price: 139.97,
+    size: "large",
+    productID: "wmen-11",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/0e818345-b196-4d34-b452-c8b48e78c4c6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "SEBBY",
+    name: "Water-Resistant Hooded Double-Breasted Trench Coat",
+    price: 59.97,
+    size: "large",
+    productID: "wmen-12",
+    category: "jackets",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/ffb0bdc4-02fa-4a06-ba0d-7e4064c8e403.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "MAGGY LONDON",
+    name: "Ruffle Sleeveless Jumpsuit",
+    price: 64.99,
+    size: "medium",
+    productID: "wmen-13",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/50deaa5a-8b8e-42fa-85ae-2b9085e6a04c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "ALEXIA ADMOR",
+    name: "Draped One-Shoulder Jumpsuit",
+    price: 69.97,
+    size: "medium",
+    productID: "wmen-14",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/6d327be2-8540-4050-817a-b04693092a38.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "MARINA",
+    name: "Beaded Sleeve Wrap Jumpsuit",
+    price: 99.97,
+    size: "large",
+    productID: "wmen-15",
+    category: "suits",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/1f2b1a80-1272-4ce2-8936-023cb975d54b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "FLORA BY FLORA NIKROOZ",
+    name: "Piped Long Sleeve Button Front Top & Pants Set",
+    price: 22.48,
+    size: "medium",
+    productID: "wmen-16",
+    category: "sleepwear",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/a5f6496d-62d3-4ab8-a941-2c89d513d03e.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "HONEYDEW INTIMATES",
+    name: "HONEYDEW Lace Trim Racerback Tank & Pants 2-Piece Pajama Set",
+    price: 22.97,
+    size: "small",
+    productID: "wmen-17",
+    category: "sleepwear",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/46b52b56-243e-4d3c-ae3b-5e0bf938cf03.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "FLORA NIKROOZ",
+    name: "Payton Robe, Tank Top, & Pants Travel 3-Piece Pajama Set",
+    price: 24.97,
+    size: "medium",
+    productID: "wmen-18",
+    category: "sleepwear",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/e8147dcf-75b9-41e7-b280-58d6857f13c9.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "FREE PEOPLE",
+    name: "Colby Raw Edge Top",
+    price: 24.99,
+    size: "medium",
+    productID: "wmen-19",
+    category: "tops",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/77c94d3d-395e-4ec5-8a0c-c08260e80c20.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+  {
+    brand: "FREE PEOPLE",
+    name: "Say the Word Floral Cotton & Modal Knit Top",
+    price: 22.97,
+    size: "small",
+    productID: "wmen-20",
+    category: "tops",
+    images:
+      "https://n.nordstrommedia.com/id/sr3/11bae0e3-7ff6-496b-8d52-3aae66c01b60.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+  },
+];
